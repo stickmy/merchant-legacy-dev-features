33103ceafb17fca56662649919437137c0dc942f
子肃 <zisu.cx@alibaba-inc.com>
2022-12-05 23:47:51 +0800

    version 1.0.0

---
+++ src/gather/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+# source nvm
+. ${NVM_DIR}/nvm.sh
+
+# nodejs 12 (@ali/gather required)
+nvm install 12
+
+# set ali registry
+yarn config set registry https://registry.npm.alibaba-inc.com/
+
+# npm@8 gather installation
+yarn global add tnpm@8 @ali/gather
